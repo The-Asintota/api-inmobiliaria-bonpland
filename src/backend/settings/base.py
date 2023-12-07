0d63e6c57fbe04cc4461410659888f133287f81a
The-Asintota <aguizandre.2803@gmail.com>
2023-12-07 17:45:49 -0500

    chore(cors): add django-cors-headers for CORS management.
    A new package, 'django-cors-headers', has been installed to handle Cross-Origin Resource Sharing (CORS) in the Django REST API. This is essential for the real estate management system to allow or restrict requested resources on a web server depending on where the HTTP request was initiated.

--- src/backend/settings/base.py
+++ src/backend/settings/base.py
@@ -26,12 +26,14 @@ LOCAL_APPS = []
 
 THIRD_APPS = [
     'rest_framework',
+    'corsheaders',
 ]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
