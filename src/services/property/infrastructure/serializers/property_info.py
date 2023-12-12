f3d586bff9d3df873fd58767c3fd0d8549878d0f
The-Asintota <aguizandre.2803@gmail.com>
2023-12-12 18:46:30 -0500

    fix(property): address PropertySerializer unexpected behavior
    - Enhance serializer reliability for property data serialization.

--- src/services/property/infrastructure/serializers/property_info.py
+++ src/services/property/infrastructure/serializers/property_info.py
@@ -1,23 +1,25 @@
 from typing import Any, Dict
 from rest_framework import serializers
 from services.property.models.constants import PropertyType
+import copy
 
 
 class PropertySerializer(serializers.Serializer):
     
     # Methods
-    def to_representation(self, instance:Dict) -> Dict[str, Any]:
-        property_id=instance.pop('id')
-        short_description=instance.pop('short_description')
-        long_description=instance.pop('long_description')
-        type_property=instance.pop('type_property')
-        del instance['date_joined']
+    def to_representation(self, instance:Dict[str, Any]) -> Dict[str, Any]:
+        data_copy= copy.copy(instance)
+        property_id=data_copy.pop('id')
+        short_description=data_copy.pop('short_description')
+        long_description=data_copy.pop('long_description')
+        type_property=data_copy.pop('type_property')
+        del data_copy['date_joined']
         data={
             'id':property_id,
             'short_description':short_description,
             'long_description':long_description,
             'type_property':type_property,
-            'features':instance,
+            'features':data_copy,
         }
         return data
 
