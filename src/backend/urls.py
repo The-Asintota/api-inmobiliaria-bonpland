d6216532aafd1c35aa883c9134803ddc6f8ab8b8
The-Asintota <aguizandre.2803@gmail.com>
2023-12-22 14:18:02 -0500

    feat(user): add user service and initial configurations
    This commit adds a new service called user, which will contain all the functionalities related to a user, such as authentication, authorization, profile, etc.
    
    This commit also adds the initial configurations of the user service, such as the models, serializers, views, and urls. The configurations are based on the requirements and specifications of the project.

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -24,6 +24,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('services.property.infrastructure.urls')),
+    path('api/v1/', include('services.user.infrastructure.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
