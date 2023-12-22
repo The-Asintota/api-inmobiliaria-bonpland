4a61245a02fcdba82574d3e80ab56c99c1c751e8
The-Asintota <aguizandre.2803@gmail.com>
2023-12-21 19:46:05 -0500

    refactor(property): use Pymongo to query MongoDB
    This change replaces the previous database access layer with Pymongo, a Python driver for MongoDB. This allows the repository module to interact with MongoDB more efficiently and flexibly, as well as to take advantage of the features and benefits of MongoDB as a NoSQL database.

--- src/services/property/models/repository/property_repository.py
+++ src/services/property/models/repository/property_repository.py
@@ -1,41 +1,25 @@
-from typing import Optional
-from django.db.models import Model
-from services.property.models import Home,Department,Local
-from services.property.models.constants import PropertyType
+from typing import Optional, Dict, Any
+from services.property.models.mongo_db import MongoModel
+from services.property.models import Properties
+from .base_repository import BaseRepository
+from bson.objectid import ObjectId
 
 
-class PropertyRepository:
+class PropertyRepository(BaseRepository):
     """
-    Encapsulates the CRUD operations of models Home, Department and Local.
+    `PropertyRepository` is a class that encapsulates CRUD queries related to the `properties` collection.
     """
     
-    _model_home=Home
-    _model_department=Department
-    _model_local=Local
-    _model_fields={
-        PropertyType.HOME.value:[field.name for field in _model_home._meta.get_fields()],
-        PropertyType.DEPARTMENT.value:[field.name for field in _model_department._meta.get_fields()],
-        PropertyType.LOCAL.value:[field.name for field in _model_local._meta.get_fields()],
-    }
+    collection: MongoModel = Properties
     
-    
-    def get_model_instance(self, type_property:str) -> Model:
+    @classmethod
+    def get_property_by_pk(cls, pk: ObjectId) -> Optional[Dict[str, Any]]:
         """
-        Get the model class based on the type_property.
+        Returns a property based on its primary key, if not found it returns None.
         """
         
-        models={
-            PropertyType.HOME.value:self._model_home,
-            PropertyType.DEPARTMENT.value:self._model_department,
-            PropertyType.LOCAL.value:self._model_local,
-        }
-        return models.get(type_property)
-    
-    
-    def get_property(self, type_property:str, id:str) -> Optional[Model]:
-        """
-        Get a property by type and UID.
-        """
-        
-        model=self.get_model_instance(type_property)
-        return model.objects.filter(id=id).values().first()
+        document = cls.collection.get_collection_refer().find_one(
+            {'pk': pk},
+            projection={'_id': 0},
+        )
+        return cls.set_document(document) if document else None
