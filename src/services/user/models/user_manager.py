3dd06b36d51b8bf66c9c98abb952c431ffe3ab77
The-Asintota <aguizandre.2803@gmail.com>
2023-12-22 18:10:48 -0500

    feat(Users): add Users model with initial fields and configurations
    This commit adds a new model called Users to the API, which represents the users of the real estate management system. The Users model has the following fields and configurations:
    
    - id: a unique UUID that identifies the user
    - dni: a string that stores the user's DNI, can be null
    - full_name: a string that stores the user's full name, can be null
    - email: a string that stores the user's email address, must be unique and not null
    - phone_number: a string that stores the user's phone number, can be null
    - password: a hashed string that stores the user's password, not null
    - is_staff: a boolean that indicates if the user is a staff member
    - is_superuser: a boolean that indicates if the user is a superuser
    - is_active: a boolean that indicates if the user is active
    - date_joined: a datetime that stores the date and time when the user joined the system
    
    The Users model also inherits from the AbstractBaseUser and PermissionsMixin classes from the Django auth module, which provide some common functionality and methods for user authentication and authorization. The model uses a custom manager, UserCustomManager, for object creation and query operations.

---
+++ src/services/user/models/user_manager.py
@@ -0,0 +1,100 @@
+from django.contrib.auth.models import UserManager, AbstractUser
+from uuid import UUID
+
+
+class UserCustomManager(UserManager):
+    """
+    Custom user manager where email is the unique identifiers for authentication
+    instead of usernames.
+    """
+
+    def _create_user(self,
+        pk: UUID = None,
+        dni: str = None,
+        full_name: str = None,
+        email: str = None,
+        phone_number: str = None,
+        password: str = None,
+        **extra_fields
+    ) -> AbstractUser:
+        user: AbstractUser = self.model(
+            pk=pk,
+            dni=dni,
+            full_name=full_name,
+            email=self.normalize_email(email),
+            phone_number=phone_number,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self,
+        pk: UUID = None,
+        dni: str = None,
+        full_name: str = None,
+        email: str = None,
+        phone_number: str = None,
+        password: str = None,
+        **extra_fields
+    ) -> AbstractUser:
+        """
+        Create and save a User with the given email and password.
+
+        Args:
+        - pk (UUID, optional): User's unique identifier. Defaults to None.
+        - dni (str, optional): User's DNI. Defaults to None.
+        - full_name (str, optional): User's full name. Defaults to None.
+        - email (str, optional): User's email. Defaults to None.
+        - phone_number (str, optional): User's phone number. Defaults to None.
+        - password (str, optional): User's password. Defaults to None.
+        - **extra_fields: Additional fields to set on the user.
+
+        Returns:
+            User: The created user instance.
+        """
+
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", False)
+        return self._create_user(
+            pk=pk,
+            dni=dni,
+            full_name=full_name,
+            email=email,
+            phone_number=phone_number,
+            password=password,
+            **extra_fields
+        )
+
+    def create_superuser(self,
+        pk: UUID = None,
+        dni: str = None,
+        full_name: str = None,
+        email: str = None,
+        phone_number: str = None,
+        password: str = None,
+        **extra_fields
+    ) -> AbstractUser:
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        elif extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        elif extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True.")
+        return self._create_user(
+            pk=pk,
+            dni=dni,
+            full_name=full_name,
+            email=email,
+            phone_number=phone_number,
+            password=password,
+            **extra_fields
+        )
